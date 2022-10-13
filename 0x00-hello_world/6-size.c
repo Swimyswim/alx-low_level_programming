3302a4490a483a48037cc32663e16be5b393d9b1
nat <natadjadeh@gmail.com>
2022-10-13 04:03:32 -0700

    alx-low_level_programming commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * Code that prints size
- * Return value is 0
+ * main - Code that prints size
+ * Return: value is 0 if code executed properly, else non zero value
  */
 
 int main(void)
@@ -20,7 +20,7 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", sizeof(e)):
 
-	return(0);
+	return (0);
 
 }
 
