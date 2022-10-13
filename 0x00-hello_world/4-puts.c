fed0dc1963919377d844fd60aecb0b47d66c6b1c
nat <natadjadeh@gmail.com>
2022-10-13 03:31:56 -0700

    alx-low_level_programming commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Code that prints a specific text
+ * with 0 as the return value
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * Code that prints size
+ * Return value is 0
+ */
+
+int main(void)
+
+{
+	char a;
+	int b;
+	long c;
+	long long d;
+	float e;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", sizeof(e)):
+
+	return(0);
+
+}
+
+
