0b57bbdaf7a90dafa22be462763dbca8ca89588b
nat <natadjadeh@gmail.com>
2022-10-14 12:51:55 -0700

    Corrections

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,6 +10,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is %d ", n, n % 10);
@@ -19,5 +20,5 @@ int main(void)
 		printf("and is 0\n");
 	else if (n % 10 != 0 && n % 10 < 6)
 		printf("and is less than 6 and not 0\n");
-	 return (0);
-}	
+       	return (0);
+}
