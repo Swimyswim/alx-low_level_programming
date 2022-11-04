a5c166577b5a7f548e4fa304fa0e4c07cbb4b1df
nat <natadjadeh@gmail.com>
2022-11-04 09:21:49 -0700

    0x09-static_libraries commit

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - converst character to strings
+ * %d: value
+ * i: calue
+ * @*s: value
+ * Return: results
+ */
+
+int main(void)
+{
+	int i;
+	char *s;
+
+	s = " -9885";
+	i = atoi(s);     /* i = -9885 */
+
+	printf("i = %d\n", i);
+
+}
